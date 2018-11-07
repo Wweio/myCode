bd81c155ae2747482542cd6beb0d257242f35bdd
vv <wei.wei@ecreditpal.com>
2018-11-07 18:05:56 +0800

    34234

--- Bra/spiders/bra.py
+++ Bra/spiders/bra.py
@@ -28,10 +28,10 @@ class BraSpider(scrapy.Spider):
     def start_requests(self):
         for page in range(1, 100):
             url = self.base_url % page
-            # print(url)
+            print(url)
             self.headers[':path'] = url
             yield Request(url, self.parse, headers=self.headers)
-            # time.sleep(2)
+            time.sleep(2)
 
     def parse(self, response):
         content = json.loads(response.text)
